1adc3e4e98f098ca7fa5a264b404ec1ebde7550e
DESKTOP-CGIQ425\Djordje <djordjeivanovic032@gmail.com>
2018-02-08 22:22:34 +0100

    Image upload as multipart data
    Former-commit-id: 2fc175ddda767e94f5641a6bafa740481a6fbbe9

---
+++ android-develop/ServerConnection/app/src/main/java/com/example/djordje/serverconnection/Constants.java
@@ -0,0 +1,12 @@
+package com.example.djordje.serverconnection;
+
+/**
+ * Created by Djordje on 08-Feb-18.
+ */
+
+public class Constants {
+
+    public static final String UPLOAD_URL = "http://192.168.106.1/AndroidImageUpload/upload.php";
+    public static final String IMAGES_URL = "http://192.168.106.1/AndroidImageUpload/getImages.php";
+}
+
