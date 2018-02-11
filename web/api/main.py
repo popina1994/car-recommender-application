a961524fff048fbb139a0b33feacb6e26070abaf
Nemanja Mikic <mn130226d@student.etf.rs>
2018-02-11 03:48:31 +0100

    Error mappers added
    Former-commit-id: 9356105f20878f19089f3aa10edddc6ea50cfa12

--- web/api/main.py
+++ web/api/main.py
@@ -1,11 +1,12 @@
 import os
 import uuid
-from flask import Flask, request, json, Response
-from werkzeug.utils import secure_filename
+
+from flask import Flask, request, jsonify
+
+from exceptions import HTTPException, HTTPBadRequest, HTTPPayloadTooLarge, HTTPMessage
 from machine_learning.inception import checkpoint_model_api
 
 app = Flask(__name__)
-UPLOAD_FOLDER = os.path.join('data')
 ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 # Log directory where checkpoint model is saved
@@ -20,37 +21,37 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+@app.errorhandler(HTTPException)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+@app.errorhandler(413)
+def error413(_):
+    response = jsonify(HTTPPayloadTooLarge(HTTPMessage.FILE_IS_TOO_LARGE).to_dict())
+    response.status_code = 413
+    return response
+
 @app.route("/v1/rpc/upload", methods=['POST'])
 def upload_image():
     # check if the post request has the file part
-    if 'file' not in request.files:
-        resp = Response(json.dumps({'error': 'There is no file part'}))
-        resp.headers['Content-Type'] = 'application/json'
-        resp.status_code = 400
-        return resp
+    if 'file' not in request.files or request.files['file'].filename == '':
+        raise HTTPBadRequest(HTTPMessage.MISSING_FILE_PART)
     file = request.files['file']
-    # if user does not select file, browser also
-    # submit a empty part without filename
-    if file.filename == '':
-        resp = Response(json.dumps({'error': 'There is no file'}))
-        resp.headers['Content-Type'] = 'application/json'
-        resp.status_code = 400
-        return resp
+
     if file and allowed_file(file.filename):
         filename = str(uuid.uuid4().hex) + '.' + file.filename.rsplit('.', 1)[1].lower()
-        file.save(os.path.join(os.getcwd(), 'web', app.config['UPLOAD_FOLDER'], filename))
-        pred = app.config['MODEL'].predict(image_path=os.path.join(os.getcwd(), 'web', app.config['UPLOAD_FOLDER'], filename))
-        resp = Response(json.dumps({'car_type': str(pred)}))
-        resp.headers['Content-Type'] = 'application/json'
-        return resp
-    resp = Response(json.dumps({'error': 'There was error processing your request'}))
-    resp.headers['Content-Type'] = 'application/json'
-    resp.status_code = 400
-    return resp
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        predict = app.config['MODEL'].predict(image_path=file_path)
+        return jsonify({'car_type': str(predict)})
+    else:
+        raise HTTPBadRequest(HTTPMessage.NOT_EXTENSION_ALLOWED)
 
 
 if __name__ == '__main__':
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'web', 'data')
     app.config['SECRET_KEY'] = 'cars123'
     app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
     app.config['MODEL'] = checkpoint_model_api.ConvNetModel(checkpoint_dir=log_dir, labels_path=labels_path)
