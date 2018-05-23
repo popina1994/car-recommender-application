35d45a909068038d616590d3a7d9d4ff0b1fcc1e
Djordje Zivanovic <djordjezivanovicva@gmail.com>
2018-05-23 00:54:39 -0700

    1. Further refactoring code so it supports multiple model and sites download.
    Former-commit-id: a0a7dde3873d5b34fb581591bcf56aaa179281b9

--- scrapper/Url/Crawler/Crawler/Crawler.py
+++ scrapper/Url/Crawler/Crawler/Crawler.py
@@ -6,13 +6,12 @@ import requests
 
 HOME_PAGE_URL = ["https://www.polovniautomobili.com", "https://www.autoscout24.com"]
 SEARCH_PAGE_URL = ["https://www.polovniautomobili.com/putnicka-vozila/pretraga?page={}&sort=renewDate_desc&brand={}&model%5B0%5D={}&city_distance=0&showOldNew=all&without_price=1",
-                   "https://www.autoscout24.com/results?sort=price&desc=0&ustate=N%2CU&size=20&page={}&mmvmk0={}&mmvmd0={}&fregfrom=2016&atype=C&"]
+                   "https://www.autoscout24.com/results?sort=price&desc=0&ustate=N%2CU&page={}&mmvmk0={}&mmvmd0={}&body={}&atype=C&size=20"]
 DIV_SEARCH_PAGE = [{"class": "uk-grid uk-margin-remove"}, {"class": "cldt-summary-titles"}]
 DIV_GALLERY_PAGE = [ ("li", {"class" : "uk-text-center"}, "href"), 
                     ("div", {"class" : "gallery-picture sc-lazy-image"}, ["data-src", "src"])]
-
 DB_MODELS = {}
-IDX_LIMIT = 100
+IDX_LIMIT = 100000
 
 class ParserPage:
 
@@ -57,10 +56,10 @@ class ParserSubPage:
         return page_download
 
 class ParserModel:
-    def __init__(self, model: str, num_page: int, home_page: str, search_page: str,
+    def __init__(self, model, model_name:str, home_page: str, search_page: str,
                  div_search_page, crawl_element):
         self.model = model 
-        self.num_page = num_page
+        self.model_name = model_name
         self.home_page = home_page
         self.search_page = search_page
         self.div_search_page = div_search_page
@@ -70,9 +69,9 @@ class ParserModel:
         url_path = self.search_page
         
         images_to_download = []
-        for pg_idx in range(1, self.num_page + 1):
-            url = url_path.format(pg_idx, DB_MODELS[self.model]["company"], 
-                                  DB_MODELS[self.model]["model"])
+        for pg_idx in range(1, self.model["num_pages"] + 1):
+            url = url_path.format(pg_idx, self.model["company"], 
+                                  self.model["model"], self.model["body"])
             images_to_download += self.parse_search_page(url)
             print("Finished page{}".format(pg_idx))
         
@@ -98,19 +97,38 @@ class ParserModel:
         opener = urllib.request.build_opener()
         opener.addheaders = [('User-agent', 'Mozilla/5.0')]
         urllib.request.install_opener(opener)
-        if not os.path.exists(self.model):
-            os.makedirs(self.model)
+        if not os.path.exists(self.model_name):
+            os.makedirs(self.model_name)
 
         for idx, url_image in enumerate(images_to_download):
-            urllib.request.urlretrieve(url_image, os.path.join(self.model, str(idx) + ".jpg"))
+            try:
+                urllib.request.urlretrieve(url_image, os.path.join(self.model_name, str(idx) + ".jpg"))
+            except:
+                print("Unsuccessful download{}".format(idx))
             if (idx % 100 == 0):
                 print("Downloaded: {}/{}", idx, len(images_to_download))
 
 if __name__ == "__main__":
-    #DB_MODELS["X3"] = {"company": "37", "model" : "1054"}
-    #ParserModel(model="X3", num_page=1, home_page=HOME_PAGE_URL[0], search_page=SEARCH_PAGE_URL[0],
-    #            div_search_page=DIV_SEARCH_PAGE[0], crawl_element=DIV_GALLERY_PAGE[0]).parse()
+    t = 1
+    """   
+   DB_MODELS["X3"] = {0: {"company": "37", "model" : "1054", "num_pages" : 20}, 
+                       1:{"company": "13", "model" : "18387", "num_pages": 20}}
     
-    DB_MODELS["X3"] = {"company": "13", "model" : "18387"}
-    ParserModel(model="X3", num_page=20, home_page=HOME_PAGE_URL[1], search_page=SEARCH_PAGE_URL[1],
-                div_search_page=DIV_SEARCH_PAGE[1], crawl_element=DIV_GALLERY_PAGE[1]).parse()
+    DB_MODELS["114"] = {0: {"company": "37", "model" : "1054", "num_pages" : 1}, 
+                       1:{"company": "13", "model" : "20149", "num_pages": 20, "body":"6"}}
+    """
+
+    DB_MODELS["116"] = {0: {"company": "37", "model" : "1054", "num_pages" : 20}, 
+                       1:{"company": "13", "model" : "18480", "num_pages": 20, "body":"6"}}
+    DB_MODELS["118"] = {0: {"company": "37", "model" : "1054", "num_pages" : 20}, 
+                    1:{"company": "13", "model" : "18481", "num_pages": 20, "body":"6"}}
+    DB_MODELS["120"] = {0: {"company": "37", "model" : "1054", "num_pages" : 20}, 
+                    1:{"company": "13", "model" : "18482", "num_pages": 20, "body":"6"}}
+
+    for model_id in DB_MODELS.keys():
+        model_list =  DB_MODELS[model_id]
+        for site_id, model_site in model_list.items():
+            if site_id == 0: continue
+            ParserModel(model=model_site, model_name=model_id, home_page=HOME_PAGE_URL[site_id], 
+                        search_page=SEARCH_PAGE_URL[site_id], div_search_page=DIV_SEARCH_PAGE[site_id], 
+                        crawl_element=DIV_GALLERY_PAGE[site_id]).parse()
